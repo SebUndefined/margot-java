41198cc82c6d0751b81dc5c3f8e1d60d4dd49afa
SebUndefined <me@sebundefined.fr>
2017-11-17 16:41:06 +0100

    Fixing some menu bug

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -1,12 +1,11 @@
 $(function(){
     // Activate the side menu
     $(".button-collapse").sideNav({
-        menuWidth: 250, // Default is 300
-        edge: 'left', // Choose the horizontal origin
-        //closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        draggable: true // Choose whether you can drag to open on touch screens
-    });
-    $("#openpersonnalmenu").sideNav();
+         menuWidth: 250, // Default is 300
+         edge: 'left', // Choose the horizontal origin
+         //closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+         draggable: true // Choose whether you can drag to open on touch screens
+     });
     $('.collapsible').collapsible();
     Materialize.updateTextFields();
     $('select').material_select();
